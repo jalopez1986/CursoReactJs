0eeda24c442fb71c4edd9e6a80f665e936af8a71
Jorge A Lopez <jlobeto@gmail.com>
2020-03-03 23:48:30 -0500

    component compuesto

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React from "react";
+
+import confLogo from "../images/badge-header.svg";
 
 class Badge extends React.Component {
-    render() {
-        return (
-            <h1>Badge</h1>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <div>
+          <img src={confLogo} alt="Logo de la conferencia" />
+        </div>
+
+        <div>
+          <img
+            src="https://es.gravatar.com/avatar?d=identicon"
+            alt="Avatar"
+          ></img>
+          <h1>
+            Jorge <br /> Lopez
+          </h1>
+        </div>
+
+        <div>
+          <p>Software Engineering</p>
+          <p>@jlo_beto</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Badge;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import Badge from './components/Badge';
+import Badge from "./components/Badge";
 
-const container = document.getElementById('app');
+const container = document.getElementById("app");
 
 ReactDOM.render(<Badge />, container);
-

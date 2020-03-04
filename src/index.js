23773b86e56631f824f9b54aed34b47e7473641e
Jorge A Lopez <jlobeto@gmail.com>
2020-03-03 22:54:34 -0500

    React.create Element

--- src/index.js
+++ src/index.js
@@ -8,9 +8,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello, Platzi Badges!</h1>;
+const jsx = <h1>Hello, Platzi Badges!</h1>;
+//hay una alternativa usando React.createElement
+const element = React.createElement('h1', {}, "Hola React CreateElement");
+const elementWithProps = React.createElement('a', {href: 'https://platzi.com'}, 'Ir a platzi');
 
 const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+
+ReactDOM.render(elementWithProps, container);
+

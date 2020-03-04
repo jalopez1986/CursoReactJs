b4a8ce5b8e31445c83a9e9fdb6e6abea35453663
Jorge A Lopez <jlobeto@gmail.com>
2020-03-03 23:21:46 -0500

    Primer componente simple

--- src/index.js
+++ src/index.js
@@ -1,23 +1,9 @@
-// const element = document.createElement('h1');
-// element.innerText = 'Hello, Platzi Badges!';
-
-// const container = document.getElementById('app');
-
-// container.appendChild(element);
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//const jsx = <h1>Hello, Platzi Badges!</h1>;
-//si queremos algo variable
-const name = 'Jorge';
-const sum = () => 3 + 3
-const jsx = <h1>Hello, Soy {name} mi edad {30 + 3} llamo funcion {sum()}</h1>;
-
+import Badge from './components/Badge';
 
 const container = document.getElementById('app');
 
-// ReactDOM.render(__qué__, __dónde__);
-
-ReactDOM.render(jsx, container);
+ReactDOM.render(<Badge />, container);
 

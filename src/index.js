4c53e478fe66af240f51733bae1cc0601a10cd11
Jorge A Lopez <jlobeto@gmail.com>
2020-03-05 23:11:13 -0500

    Primer pagina con dos components

--- src/index.js
+++ src/index.js
@@ -3,17 +3,8 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "./global.css";
 
-import Badge from "./components/Badge";
+import BadgeNew from "./pages/BadgeNew";
 
 const container = document.getElementById("app");
 
-ReactDOM.render(
-  <Badge
-    firstName="Carolina"
-    lastName="Gonzalez"
-    avatarUrl="https://es.gravatar.com/avatar?d=identicon"
-    jobTitle="Software Engineer"
-    twitter="jlobeto"
-  />,
-  container
-);
+ReactDOM.render(<BadgeNew />, container);
---
+++ src/pages/BadgeNew.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+import "./styles/BadgeNew.css";
+import header from "../images/badge-header.svg";
+import Navbar from "../components/Navbar";
+import Badge from "../components/Badge";
+
+class BadgeNew extends React.Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div className="BadgeNew__hero">
+          <img className="img-fluid" src={header} alt="Logo" />
+        </div>
+
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <Badge
+                firstName="Jorge"
+                lastName="Lopez"
+                twitter="sparragus"
+                jobTitle="Software Engineer"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BadgeNew;

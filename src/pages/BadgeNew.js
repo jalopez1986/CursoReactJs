d2c7d7621dfc3cbfc774395782d740ff2a388ab7
Jorge A Lopez <jlobeto@gmail.com>
2020-03-08 18:39:44 -0500

    Levantamiento del estado

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -7,6 +7,25 @@ import Badge from "../components/Badge";
 import BadgeForm from "../components/BadgeForm";
 
 class BadgeNew extends React.Component {
+  state = {
+    form: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      jobTitle: "",
+      twitter: ""
+    }
+  };
+
+  handleChange = e => {
+    this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
   render() {
     return (
       <div>
@@ -19,15 +38,19 @@ class BadgeNew extends React.Component {
           <div className="row">
             <div className="col-6">
               <Badge
-                firstName="Jorge"
-                lastName="Lopez"
-                twitter="sparragus"
-                jobTitle="Software Engineer"
+                firstName={this.state.form.firstName}
+                lastName={this.state.form.lastName}
+                twitter={this.state.form.twitter}
+                jobTitle={this.state.form.jobTitle}
+                email={this.state.form.email}
                 avatarUrl="https://es.gravatar.com/avatar?d=identicon"
               />
             </div>
             <div className="col-6">
-              <BadgeForm />
+              <BadgeForm
+                onChange={this.handleChange}
+                formValues={this.state.form}
+              />
             </div>
           </div>
         </div>

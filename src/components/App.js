3d019d0e7d7e8ccf2e57b74d19795d46970706da
Jorge A Lopez <jlobeto@gmail.com>
2020-05-25 15:13:45 -0500

    refactor componentes presentaciones y container

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Layout from "./Layout";
 import BadgeNew from "../pages/BadgeNew";
-import BadgeDetails from "../pages/BadgeDetails";
+import BadgeDetails from "../pages/BadgeDetailsContainer";
 import BadgeEdit from "../pages/BadgeEdit";
 import Badges from "../pages/Badges";
 import Home from "../pages/Home";
